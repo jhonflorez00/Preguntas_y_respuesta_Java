425021497bfc5a3da34a361ac543375b69d74290
jhon florez <jsflorez681@misena.edu.co>
2021-09-12 17:30:02 -0500

    cambien el mejase cuando ganan

--- src/comtrolador/MostrarGanador.java
+++ src/comtrolador/MostrarGanador.java
@@ -9,7 +9,7 @@ public class MostrarGanador {
     public  static void mostrarGanador(List<Premio> premios){
 
         Premio premio=premios.stream().max((a,b)->a.getResultadoPremio().compareTo(b.getResultadoPremio())).get();//comparar la consult
-        System.out.println("el ganador es :"+ premio.getJugador().getNombre()+"\n" +
+        System.out.println("el primer fe ganador:"+ premio.getJugador().getNombre()+"\n" +
                 "con un Premio: "+premio.getResultadoPremio());
     }
 }
