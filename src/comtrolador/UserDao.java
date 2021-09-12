06658e76508eb56d1f8999fe16198775bea08778
jhon florez <jsflorez681@misena.edu.co>
2021-09-11 20:47:17 -0500

    creamos el Metodo historial en UserDao para traer los datos de las base de datos para ser vistos

--- src/comtrolador/UserDao.java
+++ src/comtrolador/UserDao.java
@@ -5,6 +5,7 @@ import modelo.Conexion;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -21,12 +22,36 @@ public class UserDao {
                 ps.setInt(2,user.getCc());
                 ps.setInt(3,user.getAcomulado());
                 ps.executeUpdate(); //actualizar los cambios
-                System.out.println("Usuario Creado");
+                System.out.println("Usuario guardado");
 
             }catch (Exception ex){
                 System.out.println(ex);
             }
         }catch (SQLException e){
+            System.out.println(e);
+        }
+    }
+
+    public static void  historial(){
+        Conexion db_connect =new Conexion();
+        PreparedStatement ps=null;
+        ResultSet rs=null;
+
+        try(Connection conexion = db_connect.get_connection()){
+            String query="SELECT * FROM usuario";
+            ps=conexion.prepareStatement(query);
+            rs= ps.executeQuery();
+
+            //iesntras rs tenga datos
+            while (rs.next()){
+                System.out.println("ID        : "+rs.getInt("id"));
+                System.out.println("Nombre    : "+rs.getString("nombre"));
+                System.out.println("Cc        : "+rs.getString("cc"));
+                System.out.println("Acomulado : "+rs.getString("acomulado")+" $");
+                System.out.println("");
+            }
+        }catch (SQLException e){
+            System.out.println("No se pudieron traer los mensajes");
             System.out.println(e);
         }
     }
