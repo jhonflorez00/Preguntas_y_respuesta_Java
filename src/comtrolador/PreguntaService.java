a54aae12bf2c0801ca412dbf81333908fa78be81
jhon florez <jsflorez681@misena.edu.co>
2021-09-11 20:32:37 -0500

    le dimos formato  alo qu ese muestra

--- src/comtrolador/PreguntaService.java
+++ src/comtrolador/PreguntaService.java
@@ -17,16 +17,16 @@ public class PreguntaService {
         System.out.println("**          Ingrese su Pregunta            **");
         System.out.println("*********************************************");
         String pregunt= sc.nextLine();
-        System.out.println("Ingrese su Opciones 1- opcio1  2- opcion2 3- opcion3 3- opcio4");
+        System.out.println("Ingrese su Opciones: 1) opcio1 \\r\\n 2) opcion2 \\r\\n 3) opcion3\\r\\n 3) opcio4");
         String opcion=sc.nextLine();
-        System.out.println("**  Ingrese el numero de la respuesta 1 o 2 etc  **");
+        System.out.println("**  Ingrese el numero de la respuesta 1 ,2,3 o 4  **");
         int respuest=sc.nextInt();
         System.out.println("Igrese en numero aque categoria pertenece \n1. nive1\n2. nive2\n3. nive3\n4. nive4\n5. nivel5");
         int categori= sc.nextInt();
 
         System.out.println("*********************************************");
-        System.out.println("**         1-  Registro                    **");
-        System.out.println("**         2- Salir                         **");
+        System.out.println("**     1-  Registrar otra pregunta         **");
+        System.out.println("**     2- Salir                            **");
         System.out.println("*********************************************");
         int salir= sc.nextInt();
         if (salir==1){
